f9ce07b73b78e0177119f1907079351a0a556442
Reynard the Fox <51772084+reynard-fox@users.noreply.github.com>
2022-03-27 23:49:13 -0400

    Update Text mine NYC Jobs for Top Skills.R

--- Text mine NYC Jobs for Top Skills.R
+++ Text mine NYC Jobs for Top Skills.R
@@ -4,13 +4,24 @@
 #https://rpubs.com/tonmcg/socrata-discovery
 #https://stackoverflow.com/questions/21641522/how-to-remove-specific-special-characters-in-r/21641569
 
+#https://stackoverflow.com/questions/56955320/file-contain-u00c2-u00a0-convert-to-characters
+#https://stackoverflow.com/questions/10294284/remove-all-special-characters-from-a-string-in-r
+#https://stackoverflow.com/questions/60259657/how-to-remove-non-utf-8-characters-from-text
 
+#https://statsandr.com/blog/how-to-upload-r-code-on-github-example-with-an-r-script-on-mac-os/
 
 # Text mining NYC jobs
 # https://data.cityofnewyork.us/City-Government/NYC-Jobs/kpav-sd4t
 
+##------------------------------------------------------
+## Set up
+##------------------------------------------------------
+
+
 # Clean up
 rm(list=ls())
+
+`%!in%` <- Negate(`%in%`)
 
 library(RSocrata)
 library(dplyr)
@@ -19,40 +30,50 @@ library(tidyverse)
 # library(jsonlite)
 library(lubridate)
 
+
 # Load data
 nyc_jobs <- "https://data.cityofnewyork.us/resource/kpav-sd4t.json?$select=*"
 nyc_jobs <- read.socrata("https://data.cityofnewyork.us/api/odata/v4/kpav-sd4t",stringsAsFactors=FALSE)
+
 
 # Set working directory
 script_name <- rstudioapi::getSourceEditorContext()$path
 setwd(dirname(script_name))
 
 
-# prep for analysis
-nyc_jobs$floor.date <- floor_date(nyc_jobs$posting_date, "month")
+# Prep data for analysis
+nyc_jobs$floor.month <- floor_date(nyc_jobs$posting_date, "month")
 nyc_jobs$career_level <-ifelse(is.na(nyc_jobs$career_level) | nyc_jobs$career_level == "", "Unknown",nyc_jobs$career_level)
+
+##------------------------------------------------------
+## Job postings by date, agency, and career level
+##------------------------------------------------------
 
 
 # DATA: total job postings by month
 job_postings_by_month <- nyc_jobs %>% 
-  select(floor.date,number_of_positions) %>%
-  group_by(floor.date) %>%
+  select(floor.month,number_of_positions) %>%
+  group_by(floor.month) %>%
   summarise(new_postings_count = sum(n()),
             total_postings_count = sum(number_of_positions)) %>%
   mutate(new_postings_total = cumsum(new_postings_count),
-          total_postings_total = cumsum(total_postings_count))
+         total_postings_total = cumsum(total_postings_count),
+         floor.month = as.Date(floor.month))
 
 
 # LINE GRAPH: postings each month (distinct, cumulative)
-ggplot(job_postings_by_month, aes(x = floor.date)) +
+ggplot(job_postings_by_month, aes(x = floor.month)) +
   geom_line(aes(y = new_postings_count),color = "darkred") +
-  geom_line(aes(y = new_postings_total),color = "darkblue") 
-  
+  geom_line(aes(y = new_postings_total),color = "darkblue") +
+  theme(legend.position = "bottom",axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  scale_x_date(breaks = "6 month", minor_breaks = "3 month",date_labels = "%m-%Y") +
+  labs(title = "Total Job Postings each Month Agency", x = "Month", y = "Agency") 
+
 
 # DATA: total job postings by agency
 job_postings_by_agency <- nyc_jobs %>% 
-  select(floor.date,agency) %>%
-  group_by(floor.date,agency) %>%
+  select(floor.month,agency) %>%
+  group_by(floor.month,agency) %>%
   summarise(new_postings_count = sum(n())) %>%
   mutate(new_postings_total = cumsum(new_postings_count))
 
@@ -63,7 +84,7 @@ job_postings_by_agency %>% group_by(agency) %>%
   ggplot(aes(x=reorder(agency,new_postings_count),y=new_postings_count, fill=agency)) + 
   geom_bar(stat="identity") +
   geom_text(aes(label=agency), angle = 90,vjust=.5,hjust=0,size=2) +
-  guides(colour = guide_colourbar(order = 1))+
+  guides(colour = guide_colourbar(order = 1)) +
   theme(legend.position = "none",
         axis.text.x=element_blank()) +
   labs(title = "Total Job Postings by Agency", x = "Total Job Postings", y = "Agency") 
@@ -79,56 +100,48 @@ job_postings_by_exp_level_agency <- nyc_jobs %>%
   rowwise() %>% mutate(Total = rowSums(across(where(is.numeric))))
 
 
-# list of terms to search postings for
-# I removed keyword "R" - too many false hits
-system <- c("R",	"R",	"R",	"R",	"R",	"R",	"R",	"R",	"R",	"R",	"R",	"R")
-key_word <- c("RStudio",	"Rstudio",	"R,",	" R,",	" R ",	"DBI",	"tidyverse",	"dplyr",	"tidyr",	"ggplot2",	"ggplot",	"ggmap")
-system_words <- as.data.frame(cbind(system,key_word))
-
-system <- c("Python",	"Python",	"Python",	"Python",	"Python",	"Python",	"Python",	"Python",	"Python",	"Python",	"Python")
-key_word <- c("Python",	"Anaconda",	"Pycharm",	"PyCharm",	"Pandas",	"SciPy",	"Numpy",	"Matplotlib",	"Seaborn",	"Scikit-Learn",	"TensorFlow")
-temp <- as.data.frame(cbind(system,key_word))
-system_words <- rbind(system_words,temp)
-
-system <- c("SQL",	"SQL",	"SQL",	"SQL",	"SQL",	"SQL",	"SQL",	"SQL",	"SQL",	"SQL",	"SQL",	"SQL",	"SQL",	"SQL",	"SQL")
-key_word <- c("SQL",	"T-SQL",	"Db2",	"Oracle",	"IBM Db2",	"SQLite",	"MySQL",	"Microsoft SQL Server",	"SQL Server",	"SSMS",	"PostgreSQL",	"pgAdmin",	"sql server management studio",	"SQL server management studio",	"SQL Server Management Studio")
-temp <- as.data.frame(cbind(system,key_word))
-system_words <- rbind(system_words,temp)
-
-system <- c("Excel",	"Excel",	"Excel",	"Excel",	"Excel",	"Excel",	"Excel")
-key_word <- c("Excel",	"MS Excel",	"M.S. Excel",	"Microsoft Excel",	"Piivot Tables",	"PivotTables",	"VBA")
-temp <- as.data.frame(cbind(system,key_word))
-system_words <- rbind(system_words,temp)
-
-system <- c("Access",	"Access",	"Access",	"Access",	"Access")
-key_word <- c("MS Access",	"M.S. Access",	"Access Forms",	"Access",	"Microsoft Access")
-temp <- as.data.frame(cbind(system,key_word))
-system_words <- rbind(system_words,temp)
-
-# I removed keyword "MS"
-system <- c("Microsoft",	"Microsoft",	"Microsoft")
-key_word <- c("Microsoft",	"MSFT",	"Msft")
-temp <- as.data.frame(cbind(system,key_word))
-system_words <- rbind(system_words,temp)
-rm(temp)
-
-# ACCESS & EXCEL MUST have a match on Microsoft to count
-microsoft <- system_words %>% filter(system=="Microsoft") %>% select(key_word) %>% unlist()
-ms_access <- system_words %>% filter(system=="Access") %>% select(key_word) %>% unlist()
-ms_excel <- system_words %>% filter(system=="Excel") %>% select(key_word) %>% unlist()
-sql <- system_words %>% filter(system=="SQL") %>% select(key_word) %>% unlist()
-python <- system_words %>% filter(system=="Python") %>% select(key_word) %>% unlist()
-rstudio <- system_words %>% filter(system=="R") %>% select(key_word) %>% unlist()
-
+##------------------------------------------------------
+## Preferred skill KEY WORDS
+##------------------------------------------------------
 # prep list for matching
+# >> ACCESS & EXCEL MUST have a match on Microsoft to count
+# >> Need to add other tech skills - programming, azure/dev ops, aws, cloud
+# >> R key words - I removed keyword "R" - too many false hits
+# >> Microsoft key words - I removed keyword "MS"
+
+
+# Microsoft
+microsoft <- c("Excel",	"MS Excel",	"M.S. Excel",	"Microsoft Excel",	"Piivot Tables",	"PivotTables",	"VBA")
 t_microsoft <- paste0(unlist(microsoft),collapse = "|")
+
+
+# Access
+ms_access <- c("MS Access",	"M.S. Access",	"Access Forms",	"Access",	"Microsoft Access")
 t_ms_access <- paste0(unlist(ms_access),collapse = "|")
+
+
+# Excel
+ms_excel <- c("Excel",	"MS Excel",	"M.S. Excel",	"Microsoft Excel",	"Piivot Tables",	"PivotTables",	"VBA")
 t_ms_excel <- paste0(unlist(ms_excel),collapse = "|")
+
+
+# SQL
+sql <- c("SQL",	"T-SQL",	"Db2",	"Oracle",	"IBM Db2",	"SQLite",	"MySQL",	"Microsoft SQL Server",	"SQL Server",	"SSMS",	"PostgreSQL",	
+         "pgAdmin",	"sql server management studio",	"SQL server management studio",	"SQL Server Management Studio")
 t_sql <- paste0(unlist(sql),collapse = "|")
+
+
+# Python
+python <- c("Python",	"Anaconda",	"Pycharm",	"PyCharm",	"Pandas",	"SciPy",	"Numpy",	"Matplotlib",	"Seaborn",	"Scikit-Learn",	"TensorFlow")
 t_python <- paste0(unlist(python),collapse = "|")
+
+
+# R
+rstudio <- c("RStudio", "Rstudio", "R,", " R,", " R ", "DBI", "tidyverse", "dplyr", "tidyr", "ggplot2", "ggplot", "ggmap")
 t_rstudio <- paste0(unlist(rstudio),collapse = "|")
 
-# match
+
+# Match
 nyc_jobs <- nyc_jobs %>% 
   mutate(microsoft = if_else(str_detect(preferred_skills,t_microsoft),1,0,missing=0),
          ms_access = if_else(str_detect(preferred_skills,t_ms_access) & str_detect(preferred_skills,t_microsoft),1,0,missing=0),
@@ -170,57 +183,211 @@ nyc_jobs %>%
             python = sum(python),
             rstudio = sum(rstudio)) -> skill_agency
 
-# STOP
-###############################################################
+
+##------------------------------------------------------
+## Job postings by career level - SQL, R, PYTHON
+##------------------------------------------------------
+
 # NEXT - preferred skill analysis - how does it vary by identified key words? 
 # https://towardsdatascience.com/python-vs-r-what-i-learned-from-4-000-job-advertisements-ab41661b7f28
 
-
 # there are encoding issues to remove - these didn't work
-# nyc_jobs$preferred_skills[3] 
-# iconv(nyc_jobs$preferred_skills[3], "UTF-8", "ASCII", sub="")
-# str_squish(str_trim(iconv(test, "UTF-8", "ASCII", sub="")))
-
 # options include: latin1, ASCII, UTF-8,'ASCII//TRANSLIT'
-# str_squish(str_trim(iconv(test$preferred_skills[3], "UTF-8", "ASCII", sub="")))
 
 
-test <- nyc_jobs
+#sql, python, rstudio = SPR
+job <- nyc_jobs %>% filter(sql == 1 | python == 1 | rstudio == 1)
 
 # there are encoding issues to remove
-test[] <- lapply(test, iconv, "UTF-8", "ASCII", sub="")
-test[] <- lapply(test, str_trim)
-test[] <- lapply(test, str_squish)
+job[] <- lapply(job, iconv, "UTF-8", "ASCII", sub="")
+job[] <- lapply(job, str_trim)
+job[] <- lapply(job, str_squish)
 
-
-to_test <- test %>% 
+# select just job description stuff
+job <- job %>% 
   select(job_description,minimum_qual_requirements,preferred_skills) %>% 
   as.data.frame()
 
-
-test_job_preferred_skills <- test %>% select(preferred_skills) %>% 
+# clean the data - remove blanks/null/missing and UNNEST
+job_preferred_skills <- job %>% select(preferred_skills) %>% 
   filter(!map_lgl(preferred_skills, is.null)) %>%
   filter(!map_lgl(preferred_skills, is.na)) %>% 
   filter(preferred_skills != "") %>% unnest(preferred_skills)
 
-test_job_preferred_skills %>%
-  mutate_all(funs(gsub("[^[:alnum:] ]", "", .))) %>%
-  mutate_all(funs(gsub("[[:punct:]]", "", .))) -> test
+# remove non-alphanumeric values
+job_preferred_skills <- job_preferred_skills %>%
+  mutate(across(everything(),~ gsub("[^[:alnum:] ]", "", .))) %>%
+  mutate(across(everything(),~ gsub("[[:punct:]]", "", .)))
 
+# create TWO word pairs/ngrams
+job_preferred_skills_tokens <- job_preferred_skills %>%
+  tidytext::unnest_tokens(word, preferred_skills,token = "ngrams", n = 2) 
 
-job_preferred_skills_tokens <- test %>%
-  tidytext::unnest_tokens(word, preferred_skills,token = "ngrams", n = 2) %>%
-  dplyr::anti_join(stop_words, by = "word")
+# separate these into pairs
+job_preferred_skills_tokens <- job_preferred_skills_tokens %>%  
+  separate(word, into = c("word1", "word2"), sep = " ")
 
-job_preferred_skills_tokens %>%
+# remove ngrams with stop words in either pair
+job_preferred_skills_tokens <- job_preferred_skills_tokens %>%
+  filter(!word1 %in% stop_words$word,
+         !word2 %in% stop_words$word) %>%
+  unite(word, c(word1, word2), sep = " ")
+
+# re-create ngram pairs
+graph_min <- job_preferred_skills_tokens %>% 
+  count(word, sort = TRUE) %>% 
+  head(15) %>% select(n) %>% min()
+
+# graph
+SPR <- job_preferred_skills_tokens
+SPR %>%
   count(word, sort = TRUE) %>%
-  filter(n > 600) %>%
+  filter(n > graph_min) %>%
   mutate(word = reorder(word, n)) %>%
   ggplot(aes(n, word)) +
   geom_col() +
-  labs(y = NULL)
+  labs(title = "Python, R, & SQL Ngrams", x = "Term Freq", y = "Term")
 
 
 
 
+##------------------------------------------------------
+## Job postings by career level - EXCEL & ACCESS - EA
+##------------------------------------------------------
 
+# NEXT - preferred skill analysis - how does it vary by identified key words? 
+# https://towardsdatascience.com/python-vs-r-what-i-learned-from-4-000-job-advertisements-ab41661b7f28
+
+# there are encoding issues to remove - these didn't work
+# options include: latin1, ASCII, UTF-8,'ASCII//TRANSLIT'
+
+
+# Excel & Access = EA
+job <- nyc_jobs %>% filter(ms_excel == 1 | ms_access == 1)
+
+# there are encoding issues to remove
+job[] <- lapply(job, iconv, "UTF-8", "ASCII", sub="")
+job[] <- lapply(job, str_trim)
+job[] <- lapply(job, str_squish)
+
+# select just job description stuff
+job <- job %>% 
+  select(job_description,minimum_qual_requirements,preferred_skills) %>% 
+  as.data.frame()
+
+# clean the data - remove blanks/null/missing and UNNEST
+job_preferred_skills <- job %>% select(preferred_skills) %>% 
+  filter(!map_lgl(preferred_skills, is.null)) %>%
+  filter(!map_lgl(preferred_skills, is.na)) %>% 
+  filter(preferred_skills != "") %>% unnest(preferred_skills)
+
+# remove non-alphanumeric values
+job_preferred_skills <- job_preferred_skills %>%
+  mutate(across(everything(),~ gsub("[^[:alnum:] ]", "", .))) %>%
+  mutate(across(everything(),~ gsub("[[:punct:]]", "", .)))
+
+# create TWO word pairs/ngrams
+job_preferred_skills_tokens <- job_preferred_skills %>%
+  tidytext::unnest_tokens(word, preferred_skills,token = "ngrams", n = 2) 
+
+# seperate these into pairs
+job_preferred_skills_tokens <- job_preferred_skills_tokens %>%  
+  separate(word, into = c("word1", "word2"), sep = " ")
+
+# remove ngrams with stop words in either pair
+job_preferred_skills_tokens <- job_preferred_skills_tokens %>%
+  filter(!word1 %in% stop_words$word,
+         !word2 %in% stop_words$word) %>%
+  unite(word, c(word1, word2), sep = " ")
+
+# re-create ngram pairs
+graph_min <- job_preferred_skills_tokens %>% 
+  count(word, sort = TRUE) %>% 
+  head(13) %>% select(n) %>% min()
+
+# graph
+EA <- job_preferred_skills_tokens
+EA %>%
+  count(word, sort = TRUE) %>%
+  filter(n > graph_min) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(n, word)) +
+  geom_col() +
+  labs(title = "Excel & Accces Ngrams", x = "Term Freq", y = "Term")
+
+
+
+##------------------------------------------------------
+## Job postings by career level - NOT SQL, R, PYTHON
+##------------------------------------------------------
+
+# NEXT - preferred skill analysis - how does it vary by identified key words? 
+# https://towardsdatascience.com/python-vs-r-what-i-learned-from-4-000-job-advertisements-ab41661b7f28
+
+# there are encoding issues to remove - these didn't work
+# options include: latin1, ASCII, UTF-8,'ASCII//TRANSLIT'
+
+
+#sql, python, rstudio = NOT_SPR
+job <- nyc_jobs %>% filter(sql != 1 | python != 1 | rstudio != 1)
+
+# there are encoding issues to remove
+job[] <- lapply(job, iconv, "UTF-8", "ASCII", sub="")
+job[] <- lapply(job, str_trim)
+job[] <- lapply(job, str_squish)
+
+# select just job description stuff
+job <- job %>% 
+  select(job_description,minimum_qual_requirements,preferred_skills) %>% 
+  as.data.frame()
+
+# clean the data - remove blanks/null/missing and UNNEST
+job_preferred_skills <- job %>% select(preferred_skills) %>% 
+  filter(!map_lgl(preferred_skills, is.null)) %>%
+  filter(!map_lgl(preferred_skills, is.na)) %>% 
+  filter(preferred_skills != "") %>% unnest(preferred_skills)
+
+# remove non-alphanumeric values
+job_preferred_skills <- job_preferred_skills %>%
+  mutate(across(everything(),~ gsub("[^[:alnum:] ]", "", .))) %>%
+  mutate(across(everything(),~ gsub("[[:punct:]]", "", .)))
+
+# create TWO word pairs/ngrams
+job_preferred_skills_tokens <- job_preferred_skills %>%
+  tidytext::unnest_tokens(word, preferred_skills,token = "ngrams", n = 2) 
+
+# separate these into pairs
+job_preferred_skills_tokens <- job_preferred_skills_tokens %>%  
+  separate(word, into = c("word1", "word2"), sep = " ")
+
+# remove ngrams with stop words in either pair
+job_preferred_skills_tokens <- job_preferred_skills_tokens %>%
+  filter(!word1 %in% stop_words$word,
+         !word2 %in% stop_words$word) %>%
+  unite(word, c(word1, word2), sep = " ")
+
+# re-create ngram pairs
+graph_min <- job_preferred_skills_tokens %>% 
+  count(word, sort = TRUE) %>% 
+  head(15) %>% select(n) %>% min()
+
+# graph
+NOT_SPR <- job_preferred_skills_tokens
+NOT_SPR %>%
+  count(word, sort = TRUE) %>%
+  filter(n > graph_min) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(n, word)) +
+  geom_col() +
+  labs(title = "Job Postings WITHOUT Python, R, & SQL Ngrams", x = "Term Freq", y = "Term")
+
+
+###############################
+# STOP
+
+# things to add
+# > positive/negative sentiment
+# > average wage by skill set - excel/access, python/r/sql, all others
+# > info on experience level - does sentiment vary 
+# > what "machine learning" methods can i use to tease out info - add other vars from posting to ngrams?
+# > Write-up in Rmarkdown
